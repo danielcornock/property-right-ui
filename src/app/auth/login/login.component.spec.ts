2177decbfee43f5eabe8a5e9c021c8030c683f27
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2019-09-13 22:28:45 +0100

    #013 feat(auth): Add basic unit testing for login and register

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,25 +1,92 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let email,
+    password,
+    button,
+    errors = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      declarations: [LoginComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    email = component.loginForm.controls['email'];
+    password = component.loginForm.controls['password'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  describe('when the form values are empty', () => {
+    it('should return an error', () => {
+      expect(component.loginForm.valid).toBe(false);
+    });
+
+    it('should return an error for the email field', () => {
+      expect(email.valid).toBe(false);
+    });
+
+    it('should return an error for the password field', () => {
+      expect(password.valid).toBe(false);
+    });
+  });
+
+  describe('when the email is not valid', () => {
+    beforeEach(() => {
+      email.setValue('test');
+      errors = email.errors || {};
+    });
+    it('should have errors for email validation', () => {
+      expect(errors['email']).toBe(true);
+    });
+  });
+
+  describe('when the form is valid', () => {
+    let emailErrors, passwordErrors;
+    beforeEach(() => {
+      email.setValue('test@email.com');
+      password.setValue('password');
+    });
+
+    it('should state that the form is valid', () => {
+      expect(component.loginForm.valid).toBe(true);
+    });
+
+    describe('when the form is submitted', () => {
+      beforeEach(() => {
+        email.setValue('test@email.com');
+        password.setValue('password');
+        button = fixture.debugElement.nativeElement.querySelector(
+          '.login-form__submit'
+        );
+        button.click();
+      });
+
+      it('should call login function', () => {
+        let loginSpy = spyOn(component, 'login');
+        console.log(component.login());
+        expect(loginSpy).toHaveBeenCalled();
+      });
+    });
   });
 });
--- src/app/auth/register/register.component.spec.ts
+++ src/app/auth/register/register.component.spec.ts
@@ -1,25 +1,80 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterTestingModule } from '@angular/router/testing';
 
-describe('RegisterComponent', () => {
+fdescribe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let name,
+    email,
+    password,
+    button,
+    errors = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+      imports: [
+        ReactiveFormsModule,
+        FormsModule,
+        HttpClientModule,
+        RouterTestingModule
+      ],
+      declarations: [RegisterComponent]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    name = component.registerForm.controls['name'];
+    email = component.registerForm.controls['email'];
+    password = component.registerForm.controls['password'];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  fdescribe('when the form values are empty', () => {
+    it('should return an error', () => {
+      expect(component.registerForm.valid).toBe(false);
+    });
+
+    it('should return an error for the name field', () => {
+      expect(name.valid).toBe(false);
+    });
+
+    it('should return an error for the email field', () => {
+      expect(email.valid).toBe(false);
+    });
+
+    it('should return an error for the password field', () => {
+      expect(password.valid).toBe(false);
+    });
+  });
+
+  describe('when the email is not valid', () => {
+    beforeEach(() => {
+      email.setValue('test');
+      errors = email.errors || {};
+    });
+
+    it('should have errors for email validation', () => {
+      expect(errors['email']).toBe(true);
+    });
+  });
+
+  describe('when the password is not valid', () => {
+    beforeEach(() => {
+      password.setValue('hi');
+      errors = password.errors || {};
+    });
+    it('should have errors for email validation', () => {
+      expect(errors['password']).toBe(true);
+    });
   });
 });
