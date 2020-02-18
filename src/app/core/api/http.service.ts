e9a64550c942732e5cf4afa77c760b5350d409a0
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-18 21:56:45 +0000

    add production url and move font declaration

--- src/app/core/api/http.service.ts
+++ src/app/core/api/http.service.ts
@@ -1,23 +1,30 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { environment } from "src/environments/environment";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class HttpService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    if (environment.production) {
+      this.apiUrl = "https://property-right-api.herokuapp.com/api/v1/";
+    } else {
+      this.apiUrl = "http://localhost:2000/api/v1/";
+    }
+  }
 
-  private apiUrl = 'http://localhost:2000/api/v1/';
+  private apiUrl: string;
 
   private addAuthHeaders() {
     return new HttpHeaders({
-      Authorization: `Bearer ${localStorage.getItem('jwt_token') ||
-        sessionStorage.getItem('jwt_token')}`
+      Authorization: `Bearer ${localStorage.getItem("jwt_token") ||
+        sessionStorage.getItem("jwt_token")}`
     });
   }
 
   private processUrl(url: string): string {
-    return url[0] === '/' ? url.slice(1) : url;
+    return url[0] === "/" ? url.slice(1) : url;
   }
 
   public post(url: string, data: object) {
