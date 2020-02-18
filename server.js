83d8c808bb8ad0cf7fbce3caca34bbe758c6d969
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2020-02-18 21:41:13 +0000

    update folder name

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require("path");
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + "/dist/ng-kanban"));
+app.use(express.static(__dirname + "/dist/property-right-ui"));
 
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname + "/dist/ng-kanban/index.html"));
+  res.sendFile(path.join(__dirname + "/dist/property-right-ui/index.html"));
 });
 
 // Start the app by listening on the default Heroku port
